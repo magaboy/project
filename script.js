749693531c67bce6e3cbb92739166f175232072a
Muhammad <idmag95@gmail.com>
2020-12-17 23:02:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strick';
+
+
+
+
+
+
+let persone = {
+    name: "john",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+
+console.log(4/0);
+
+console.log('string'*9);
+
+let somthing;
+console.log(somthing);
+
+// alert("Hello World");
+
+// let answer = confirm("are you here?");
+
+// console.log(answer);
+
+let answer = prompt("are you here?");
+
+console.log(typeof(arr));
